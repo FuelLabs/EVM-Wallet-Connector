f7363ead283d9f9c7d67fb14a1cc6ab87198042d
Braqzen <103777923+Braqzen@users.noreply.github.com>
2023-09-14 16:36:21 +0100

    Add hardhat network to autorun a node for testing

--- sdk/hardhat.config.ts
+++ sdk/hardhat.config.ts
@@ -3,6 +3,11 @@ import "@nomicfoundation/hardhat-ethers";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    defaultNetwork: {
+      url: 'hardhat',
+    },
+  }
 };
 
 export default config;
