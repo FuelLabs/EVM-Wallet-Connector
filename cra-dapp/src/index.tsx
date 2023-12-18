2890217527d799cc57927fef1279e921b8685c7e
K1-R1 <k1r1.devwork@gmail.com>
2023-12-18 20:53:16 +0000

    Minimal FuelConnector refactor

--- cra-dapp/src/index.tsx
+++ cra-dapp/src/index.tsx
@@ -1,9 +1,7 @@
 import { FuelProvider } from '@fuel-wallet/react';
-import { Provider } from 'fuels';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import { EVMWalletConnector } from '@fuels/wallet-connector-evm';
+import { EVMWalletConnectorRefactor } from '@fuels/wallet-connector-evm';
 
 import './index.css';
 import App from './App';
@@ -12,39 +10,22 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-
-const createRoot = async () => {
-  const fuelProvider = await Provider.create('https://beta-4.fuel.network/graphql');
-  if (!fuelProvider) {
-    throw new Error('Provider not found');
-  }
-
-  // @ts-expect-error
-  const ethProvider = window.ethereum;
-  if (!ethProvider) {
-    throw new Error('Ethereum provider not found');
-  }
-
-  const connector = new EVMWalletConnector(ethProvider, fuelProvider);
-
-  return (
-    <React.StrictMode>
-      <FuelProvider
-        fuelConfig={{
-          devMode: true,
-          connectors: [connector],
-        }}
-      >
-         <App />
-      </FuelProvider>
-    </React.StrictMode>
-  );
-};
-
-(async () => {
-  const content = await createRoot();
-  root.render(content);
-})();
+root.render(
+  <React.StrictMode>
+    <FuelProvider
+      // devMode is used to show Connectors that are focused on development versions
+      // like Fuel Wallet Development
+      fuelConfig={{
+        devMode: true,
+        connectors: [
+          new EVMWalletConnectorRefactor()
+        ]
+      }}
+    >
+      <App />
+    </FuelProvider>
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,1 +1,2 @@
 export * from './EvmWalletConnector';
+export {EVMWalletConnectorRefactor} from './ConnectorRefactor';
